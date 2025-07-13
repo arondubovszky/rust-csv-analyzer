63b64e11cd6a3626eb536b2d49092d5e6e0bfd0e
arondubovszky <94438458+arondubovszky@users.noreply.github.com>
2025-07-13 17:38:06 +0300

    Delete target directory

--- target/debug/deps/csva-b67287e549567bc0.d
+++
@@ -1,5 +0,0 @@
-/Users/arondubovszky/rust_bs/rust-csv-analyzer/target/debug/deps/libcsva-b67287e549567bc0.rmeta: src/main.rs
-
-/Users/arondubovszky/rust_bs/rust-csv-analyzer/target/debug/deps/csva-b67287e549567bc0.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/csva-b96d5c6873725312.d
+++
@@ -1,5 +0,0 @@
-/Users/arondubovszky/rust_bs/rust-csv-analyzer/target/debug/deps/libcsva-b96d5c6873725312.rmeta: src/main.rs
-
-/Users/arondubovszky/rust_bs/rust-csv-analyzer/target/debug/deps/csva-b96d5c6873725312.d: src/main.rs
-
-src/main.rs:
